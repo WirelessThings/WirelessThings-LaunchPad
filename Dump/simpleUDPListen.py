1c3a24d868da106e54ff4d1a91799e5d0978cc29
dpslwk <dps.lwl@gmail.com>
2014-02-17 17:55:51 +0000

    Server: simple UDP in and out for none CONFIGME message seems to be working

---
+++ Dump/simpleUDPListen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Simple UDP broadcast listener for LLAP
+    
+    listen for UDP broadcasts on port 50140
+    
+    """
+import sys
+from time import time, sleep
+import os
+import socket
+
+LLAP_FROM_PORT = 50140
+LLAP_TO_PORT = 50141
+
+sock = socket.socket(socket.AF_INET, # Internet
+              socket.SOCK_DGRAM) # UDP
+
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+sock.bind(('', LLAP_FROM_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print("Got: {} from address {}".format(data, addr))
