839027b5b81ce29e26ac9f878cef4a3535e43d0a
dpslwk <dps.lwl@gmail.com>
2014-03-26 16:16:52 +0000

    Util: share the port with others

--- Dump/simpleUDPListen.py
+++ Dump/simpleUDPListen.py
@@ -18,6 +18,7 @@ sock = socket.socket(socket.AF_INET, # Internet
 
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
 sock.bind(('', LLAP_FROM_PORT))
 
