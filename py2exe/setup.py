88fcb97bd6fb2e5e6a872e15e19ecf2ab86968ac
marcosamorim <mamorim@outlook.com>
2015-08-11 15:30:43 +0100

    py2exe: update names

--- py2exe/setup.py
+++ py2exe/setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=['../LLAPConfigMeUI/LLAPConfigMe.py'],
-      console=['../LLAPServer/LLAPServer.py'],
-      data_files=[('', ['../LLAPConfigMeUI/LLAPCM_defaults.cfg', '../LLAPDevices.json', '../LLAPServer/LLAPServer.cfg', '../LLAPConfigMeUI/noun_80697_cc.gif'])],
+setup(windows=['../ConfigurationWizard/ConfigurationWizard.py'],
+      console=['../MessagingBridge/MessageBridge.py'],
+      data_files=[('', ['../ConfigurationWizard/ConfigurationWizard_defaults.cfg', '../LLAPDevices.json', '../MessagingBridge/MessageBridge.cfg', '../ConfigurationWizard/noun_80697_cc.gif'])],
 
       )
