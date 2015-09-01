a93c0eb92e2c1e30e333a07e035dfecdd83e3395
Marcos Amorim <marcos.amorim@wirelessthings.net>
2015-09-01 10:34:09 +0100

    LaunchPad: Fix on checkUpdateFailed variable

--- LaunchPad/LaunchPad.py
+++ LaunchPad/LaunchPad.py
@@ -153,6 +153,8 @@ class LaunchPad:
 
         if self.args.noupdate:
             self.checkForUpdate()
+        else:
+            self.updateCheckFailed = False
 
         self._running = True
 
@@ -271,7 +273,7 @@ class LaunchPad:
             try:
                 url = self.config.get('Update', 'updateurl') + self.config.get('Update',
                                     'updatefile').format(self.newVersion)
-                self.logger.info(" Attepmting to get file size for: {}".format(url))
+                self.logger.info("Attepmting to get file size for: {}".format(url))
                 u = urllib2.urlopen(url)
                 meta = u.info()
                 self.file_size = int(meta.getheaders("Content-Length")[0])
